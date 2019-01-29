c71d6f141cc3b79a0c0cb11287f19e580acd08d2
poojayadav62 <46997831+poojayadav62@users.noreply.github.com>
2019-01-29 13:36:33 +0530

    Update final.js

--- final.js
+++ final.js
@@ -1,4 +1,19 @@
+//********************************************************EmailBlast using node.js*******************************
+//**********************import section************************
+const winston = require('winston');
+const winstonRotator = require('winston-daily-rotate-file');
+var errorlog = require('./log/logger.js').errorlog;
+var successlog = require('./log/logger.js').successlog;
+var ob = require('./a.json');  
+var events = require('events');
+var eventEmitter = new events.EventEmitter();
+var nodemailer = require('nodemailer');
+var obj = require('./config.json');
+var loc= require('./pathconfig.json');
+var transporter = nodemailer.createTransport(obj);
+var fs = require('fs')
 //***************Login section**********************
+
 var prompt = require('prompt');
 var prompt_attributes = [
     {
@@ -17,7 +32,7 @@ prompt.start();
 prompt.get(prompt_attributes, function (err, result) {
     if (err) 
 	{
-        console.log(err);
+       errorlog.error(err);
         return 1;
     }
 	else 
@@ -28,32 +43,33 @@ prompt.get(prompt_attributes, function (err, result) {
       
     }
 });
-//**********************End section************************
 
-var ob = require('./a.json');
-var events = require('events');
-var eventEmitter = new events.EventEmitter();
-var nodemailer = require('nodemailer');
-var obj = require('./config.json');
-var winston = require('winston');
-var transporter = nodemailer.createTransport(obj);
-var fs = require('fs')
 
 //**********************here watcher monitor  mailbox *****
 
  
  function login(username,password)
  {
-		if(username==ob.user && password==ob.pass)
+		if(username==ob.user && password==ob.pass) // authentication
 		{
+			
 			console.log("wating for new file:");
-			fs.watch('./MailBox/', function (event, files)
+			fs.watch(loc.path, function (event, files)
 			 {
 			   if(event=='change')
 					{
-					var content = fs.readFileSync("./MailBox/"+files);
-					var jsonContent = JSON.parse(content);
-					sendmail_fun(files,jsonContent);
+					var content = fs.readFileSync(loc.path+files);
+						if(content.from!="" && content.to!="")
+						{
+						successlog.info("login success");
+						var jsonContent = JSON.parse(content);
+						sendmail_fun(files,jsonContent);
+						}
+						else
+						{
+							errorlog.error("address of sender or reciever missing");
+							
+						}
 					}
 
 		    });
@@ -62,55 +78,64 @@ var fs = require('fs')
  }
  //******************send mail function*********************
  
-	    function sendmail_fun(files,jsonContent)
+	  var sendMail= function(files,jsonContent)
 			{
 				transporter.sendMail(jsonContent, function(error, info){
 				if (error)
 					{
-					 console.log("error :"+error);
+ 					 errorlog.error(error);
 					 fail(files,jsonContent);
 				    } 
 				else
 				    {
-					console.log(files+" :start sending"); 
+					successlog.info("Success :mail is start sending");
 					success(files,jsonContent);
 				    }
 					});
 			}
  
  //*****************************copy operation in inbox************************
+ 
  	var copyFileInInbox = function (files,jsonContent) {
 			var myJSON = JSON.stringify(jsonContent);
-			const fs = require('fs');
+			//const fs = require('fs');
 			fs.writeFile("./Inbox/"+files, myJSON, function(err) {
 				if(err) 
 				{
-					return console.log(err);
+					return errorlog.error(err);
 				}
-				console.log(files+" : is sended");
-				console.log("wating for new mail:");
+				successlog.info("mail is sended");
+				successlog.info("wating for new mail:");
 			}); 
 		
 			
 		}
-//************************copy operation in draft********************************		
+//************************copy operation in draft********************************	
+	
 	var copyFileInDraft = function (files,jsonContent) {
 			var myJSON = JSON.stringify(jsonContent);
-			const fs = require('fs');
+			//const fs = require('fs');
 			fs.writeFile("./Draft/"+files, myJSON, function(err) {
 				if(err) {
-					return console.log(err);
+					return errorlog.error("error:"+err);
 				}
-        console.log("mail is save in draft");
+				
+                successlog.info("mail is save in draft");
+				successlog.info("wating for new mail:");
 			});
 		  
 		}
 //**********************add listner  *********************************************
-
+eventEmitter.on('sendMail', sendMail);
 eventEmitter.on('copyFileInInbox', copyFileInInbox);
 eventEmitter.on('copyFileInDraft', copyFileInDraft);
 
+
 //**********************execute listner*******************************************
+function sendmail_fun(files,jsonContent){
+  eventEmitter.emit('sendMail',files,jsonContent);	
+}
+
 
 function success(files,jsonContent){
     eventEmitter.emit('copyFileInInbox',files,jsonContent);
@@ -118,5 +143,6 @@ function success(files,jsonContent){
 
 function fail(files,jsonContent){
 eventEmitter.emit('copyFileInDraft',files,jsonContent);
-console.log("wating for new file:");
 }
+
+
